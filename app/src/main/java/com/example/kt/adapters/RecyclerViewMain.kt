35e56d1193cdb55fdfb0d28f00d3e1e0260571ea
geektech-kairat <goold1999@mail.ru>
2021-06-30 23:50:24 +0600

    asdi11

--- app/src/main/java/com/example/kt/adapters/RecyclerViewMain.kt
+++ app/src/main/java/com/example/kt/adapters/RecyclerViewMain.kt
@@ -8,10 +8,10 @@ import com.example.kt.baseClass.loadImage
 import com.example.kt.databinding.ItemRecyclerMainBinding
 import com.example.kt.models.Image
 
-class RecyclerViewMain(private val list: List<Image>) :
+class RecyclerViewMain(private val list: ArrayList<Image>) :
     RecyclerView.Adapter<RecyclerViewMain.ViewHolderMain>() {
 
-    private var _list: List<Image> = list
+    private var _list: ArrayList<Image> = list
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderMain {
@@ -33,22 +33,32 @@ class RecyclerViewMain(private val list: List<Image>) :
         return _list.size
     }
 
-    fun setFilter(newList: List<Image>){
+    fun setFilter(newList: ArrayList<Image>){
         this._list = newList
         notifyDataSetChanged()
+    }
+
+    fun delete(pos: Int){
+        this._list.removeAt(pos)
+        notifyItemRemoved(pos)
     }
 
 
 
 
-    class ViewHolderMain(private val binding: ItemRecyclerMainBinding) :
+
+   inner class ViewHolderMain(private val binding: ItemRecyclerMainBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(image: Image) {
+
             binding.imageItem.loadImage(image.url)
             binding.textViewItem.text = image.name
             binding.imageItem.setOnClickListener {
                 Toast.makeText(binding.root.context, image.name, Toast.LENGTH_SHORT).show()
             }
+            binding.imageItem.setOnLongClickListener {
+                delete(position)
+                true }
 
         }
 
